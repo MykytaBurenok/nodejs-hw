f5e4d03879e450eed3a694a454a205e018df7975
Mykyta <burenoknikitad@gmail.comgit config --global user.name Mykytagit config --global user.email burenoknikitad@gmail.com>
2026-07-10 12:54:54 +0200

    add mongodb with mongoose

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   console.error(err.message);
   res.status(500).json({ message: err.message });
 });
