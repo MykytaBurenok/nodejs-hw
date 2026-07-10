8b615351beea3967fe331ce9110c53c89c23e889
Mykyta <burenoknikitad@gmail.comgit config --global user.name Mykytagit config --global user.email burenoknikitad@gmail.com>
2026-07-10 13:29:57 +0200

    add mongodb notes crud

--- src/server.js
+++ src/server.js
@@ -1,45 +1,32 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import pinoHttp from 'pino-http';
-import pino from 'pino';
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const connectMongoDB = require('./db/connectMongoDB');
+const logger = require('./middleware/logger');
+const notFoundHandler = require('./middleware/notFoundHandler');
+const errorHandler = require('./middleware/errorHandler');
+const notesRouter = require('./routes/notesRoutes');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-const logger = pino();
-
-app.use(cors());
+app.use(logger);
 app.use(express.json());
-app.use(
-  pinoHttp({
-    logger,
-  }),
-);
+app.use(cors());
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
+app.use(notesRouter);
 
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
+app.use(notFoundHandler);
+app.use(errorHandler);
 
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
+const bootstrap = async () => {
+  await connectMongoDB();
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
 
-app.use((err, req, res) => {
-  console.error(err.message);
-  res.status(500).json({ message: err.message });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+bootstrap();
