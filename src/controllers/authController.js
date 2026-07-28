7ea3fefc0b130ee1daaeb17f9526e158f0575427
Mykyta <burenoknikitad@gmail.comgit config --global user.name Mykytagit config --global user.email burenoknikitad@gmail.com>
2026-07-28 23:23:58 +0200

    Fix http-errors import (default import in ESM)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,4 @@
-import { createHttpError } from 'http-errors';
+import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 import User from '../models/user.js';
 import Session from '../models/session.js';
