2feaa79c8e3a11c37d886dea1fbbb212bc57fcec
Mykyta <burenoknikitad@gmail.comgit config --global user.name Mykytagit config --global user.email burenoknikitad@gmail.com>
2026-07-28 23:41:02 +0200

    Fix User import (default export) in authenticate middleware

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,6 +1,6 @@
 import createHttpError from 'http-errors';
 import { Session } from '../models/session.js';
-import { User } from '../models/user.js';
+import User from '../models/user.js';
 
 export async function authenticate(req, res, next) {
   try {
