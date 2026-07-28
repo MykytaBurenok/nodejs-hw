b7f810d7d34ceeec8dc35020b97546171569441c
Mykyta <burenoknikitad@gmail.comgit config --global user.name Mykytagit config --global user.email burenoknikitad@gmail.com>
2026-07-28 21:43:06 +0200

    Add auth service (createSession, setSessionCookies) in ESM

---
+++ src/services/auth.js
@@ -0,0 +1,49 @@
+import { randomBytes } from 'crypto';
+import Session from '../models/session.js';
+import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/time.js';
+
+function generateToken() {
+  return randomBytes(32).toString('hex');
+}
+
+export async function createSession(userId) {
+  const accessToken = generateToken();
+  const refreshToken = generateToken();
+  const now = new Date();
+
+  const accessTokenValidUntil = new Date(now.getTime() + FIFTEEN_MINUTES);
+  const refreshTokenValidUntil = new Date(now.getTime() + ONE_DAY);
+
+  const session = await Session.create({
+    userId,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil,
+    refreshTokenValidUntil,
+  });
+
+  return session;
+}
+
+export function setSessionCookies(res, session) {
+  const common = {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+  };
+
+  res.cookie('accessToken', session.accessToken, {
+    ...common,
+    maxAge: FIFTEEN_MINUTES,
+  });
+
+  res.cookie('refreshToken', session.refreshToken, {
+    ...common,
+    maxAge: ONE_DAY,
+  });
+
+  res.cookie('sessionId', session._id.toString(), {
+    ...common,
+    maxAge: ONE_DAY,
+  });
+}
